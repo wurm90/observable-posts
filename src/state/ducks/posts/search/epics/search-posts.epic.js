085a5c3c428508334407416ecccfab050d04002e
Diego De Bonis <diego.debonis@globant.com>
2020-05-15 11:57:48 -0300

    add debounce

--- src/state/ducks/posts/search/epics/search-posts.epic.js
+++ src/state/ducks/posts/search/epics/search-posts.epic.js
@@ -2,7 +2,7 @@ import { ofType } from "redux-observable";
 import searchActions from "../actions";
 import { SearchActionTypes } from "../types";
 import { ajax } from "rxjs/ajax";
-import { map, switchMap } from "rxjs/operators";
+import { map, switchMap, debounceTime } from "rxjs/operators";
 import { of, concat } from "rxjs";
 
 const API = "http://localhost:4444/posts";
@@ -11,6 +11,7 @@ const search = (term) => `${API}?title_like=${encodeURIComponent(term)}`;
 const searchPostsEpic = (action$, state$) =>
   action$.pipe(
     ofType(SearchActionTypes.SEARCH_POSTS),
+    debounceTime(500),
     switchMap(({payload}) =>
       concat(
         of(searchActions.searchPostsLoading()),
